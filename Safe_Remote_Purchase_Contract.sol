3576333cca3a4a52ba086b632fc564df3aa91c78
d-invisible <108665598+d-invisible@users.noreply.github.com>
2023-11-03 11:31:02 +0530

    Create Safe_Remote_Purchase_Contract.sol

---
+++ Safe_Remote_Purchase_Contract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SafePurchase {
+    address public buyer;
+    address public seller;
+    uint256 public price;
+    bool public itemReceived;
+
+    // Other necessary variables and functions...
+}
