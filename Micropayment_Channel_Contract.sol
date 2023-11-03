f33aa50a33a6d6e5d8804f786771bf29dc340ebf
d-invisible <108665598+d-invisible@users.noreply.github.com>
2023-11-03 11:31:48 +0530

    Create Micropayment_Channel_Contract.sol

---
+++ Micropayment_Channel_Contract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MicropaymentChannel {
+    address public sender;
+    address public receiver;
+    uint256 public deposit;
+    uint256 public expiration;
+
+    // Other necessary variables and functions...
+}
