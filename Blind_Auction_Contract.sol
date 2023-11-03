b8d280e2a16b089f30ea9f7337f77c48fe5f7e50
d-invisible <108665598+d-invisible@users.noreply.github.com>
2023-11-03 11:30:13 +0530

    Create Blind_Auction_Contract.sol

---
+++ Blind_Auction_Contract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BlindAuction {
+    address public beneficiary;
+    uint256 public biddingEnd;
+    uint256 public revealEnd;
+    bool public ended;
+
+    // Other necessary variables and functions...
+}
